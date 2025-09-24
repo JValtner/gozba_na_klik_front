431b0ed129b4af79fcb70a0ccd6f294b5188bf73
Josip Valtner <josipvaltner@.com>
2025-09-24 21:11:25 +0200

    index.jsx dom change

--- gozba_na_klik/src/index.jsx
+++ gozba_na_klik/src/index.jsx
@@ -5,9 +5,7 @@ import "./styles/main.scss";
 import { UserProvider } from "./components/users/UserContext";
 import "./styles/main.scss";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-
-
+createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <UserProvider>
             <App />
