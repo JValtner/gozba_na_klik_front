14c696676a391ddb88dc9771a730a9081d9027d0
Uros <milinovicuros@gmail.com>
2025-09-24 19:30:36 +0200

    merge: changes before merge

--- gozba_na_klik/src/App.jsx
+++ gozba_na_klik/src/App.jsx
@@ -1,21 +1,16 @@
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import WelcomePage from "./components/welcome/WelcomePage";
 import RegisterUserForm from "./components/users/RegisterUserForm";
 import Header from "./components/welcome/Header";
 import Footer from "./components/welcome/Footer";
-import './styles/main.scss';
+import "./styles/main.scss";
 
 export default function App() {
   return (
-
     <Router>
       <Header />
-      <WelcomePage 
-        onLogin={handleLogin}
-        onGoToRegister={handleGoToRegister}
-      />
+      <WelcomePage onLogin={handleLogin} onGoToRegister={handleGoToRegister} />
       <main>
         <Routes>
           <Route path="/" element={<WelcomePage />} />
@@ -24,7 +19,5 @@ export default function App() {
       </main>
       <Footer />
     </Router>
-
-
   );
 }
--- gozba_na_klik/src/components/welcome/WelcomePage.jsx
+++ gozba_na_klik/src/components/welcome/WelcomePage.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function WelcomePage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <div style={{ textAlign: "center", marginTop: "50px" }}>
-            <h1>Dobrodošli u Gozba na Klik</h1>
-            <p>Prijavite se ili kreirajte nalog kako biste koristili aplikaciju.</p>
-            <button onClick={() => navigate("/register")}>
-                Registracija
-            </button>
-        </div>
-    );
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Dobrodošli u Gozba na Klik</h1>
+      <p>Prijavite se ili kreirajte nalog kako biste koristili aplikaciju.</p>
+      <button onClick={() => navigate("/register")}>Registracija</button>
+    </div>
+  );
 }
