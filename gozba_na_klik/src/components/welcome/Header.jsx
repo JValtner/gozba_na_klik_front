f5596e5e4d6d23280a147f23675a65486499e974
Josip Valtner <josipvaltner@.com>
2025-09-25 20:47:44 +0200

    Dodata funkcionalnost za profile icon

--- gozba_na_klik/src/components/welcome/Header.jsx
+++ gozba_na_klik/src/components/welcome/Header.jsx
@@ -1,11 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { useUser } from "../users/UserContext";
+import { getUserById } from "../service/userService"; 
+
 
 export default function Header() {
   const { username, userId, isAuth, logout } = useUser();
   const navigate = useNavigate();
+  const [user, setUser] = useState(null);
 
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (userId) {
+        try {
+          const existingUser = await getUserById(Number(userId))
+          if (existingUser) {
+            setUser(existingUser)
+          }
+        } catch (err) {
+          console.error("Failed to fetch user", err)
+        }
+      }
+    }
+    fetchUser()
+  }, [userId])
   const handleLogout = () => {
     logout();
     navigate("/login");
@@ -19,7 +37,18 @@ export default function Header() {
           <>
             <span>Dobrodošli, <strong>{username}</strong></span>
               <button className="profile-btn">
-                <Link to={`/profile/${userId}`}>Profil</Link>
+                <Link to={`/profile/${userId}`} className="profile-btn">
+                <img
+                  alt="Profile"
+                  className="profile-icon"
+                  src={
+                    user?.userImage
+                      ? `http://localhost:50307${user.userImage}`
+                      : "/default-profile.png"
+                  }
+                />
+              Profil
+            </Link>
               </button>
             
             <button className="logout-btn" onClick={handleLogout}>Odjava</button>
