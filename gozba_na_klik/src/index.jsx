f43aebb007300c6f436c37d160266ccbb1f4dd14
Boris Laketić <laketicboris@yahoo.com>
2025-09-25 13:04:59 +0200

    Index.jsx popravljen

--- gozba_na_klik/src/index.jsx
+++ gozba_na_klik/src/index.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles/main.scss";
 import { UserProvider } from "./components/users/UserContext";
 import "./styles/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-
-
-    <React.StrictMode>
-        <UserProvider>
-            <App />
-        </UserProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <UserProvider>
+      <App />
+    </UserProvider>
+  </React.StrictMode>
 );
-
