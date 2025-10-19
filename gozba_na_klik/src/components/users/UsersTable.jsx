e5c3fd78c370d55003d888fadc9d92a35334c950
Uros <milinovicuros@gmail.com>
2025-10-19 03:10:55 +0200

    feat: dodate servisne metode za admina i izmena dizajna admin dashboarda

--- gozba_na_klik/src/components/users/UsersTable.jsx
+++ gozba_na_klik/src/components/users/UsersTable.jsx
@@ -2,12 +2,23 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import UserTableRow from "./UserTableRow";
 import Spinner from "../spinner/Spinner";
-import { deleteUser, getAllUsers, updateUser } from "../service/userService";
+import { getAllUsers, updateUserRoleByAdmin } from "../service/userService";
 
 const UsersTable = () => {
   const [users, setUsers] = useState([]);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (error) {
+      const timer = setTimeout(() => {
+        setError(""); // uklanja error posle 5 sekundi
+      }, 5000);
+
+      // Čišćenje timeout-a ako se error promeni ili komponenta unmount-uje
+      return () => clearTimeout(timer);
+    }
+  }, [error]);
 
   const loadUsers = async () => {
     try {
@@ -23,9 +34,20 @@ const UsersTable = () => {
     const currentUser = users.find((user) => user.id === userId);
     if (!currentUser) return;
 
+    if (currentUser.role === "Admin") {
+      setError("Uloga admina se ne može menjati.");
+      return;
+    }
+
+    if (newRole === "Admin") {
+      setError("Ne možete dodeliti ulogu admina.");
+      return;
+    }
+
     const updatedUser = { ...currentUser, role: newRole };
+
     try {
-      await updateUser(userId, updatedUser);
+      await updateUserRoleByAdmin(userId, updatedUser);
       setUsers((prev) =>
         prev.map((user) => (user.id === userId ? updatedUser : user))
       );
@@ -34,17 +56,6 @@ const UsersTable = () => {
       console.error("Greska pri promeni uloge:", error);
       setError("Promena uloge nije uspela. Molimo pokusajte ponovo.");
     }
-  };
-
-  const handleDelete = async (userId) => {
-    setLoading(true);
-    try {
-      await deleteUser(userId);
-      setUsers((users) => users.filter((user) => user.id !== userId));
-    } catch (err) {
-      setError("Brisanje korisnika nije uspelo, pokusajte ponovo.");
-    }
-    setLoading(false);
   };
 
   useEffect(() => {
@@ -57,36 +68,41 @@ const UsersTable = () => {
 
   return (
     <div>
-      <div className="title-container">
-        <h1>Users</h1>
-      </div>
       <div className="error-container">
         {error && <span className="error-span show">{error}</span>}
       </div>
       <div className="table-container">
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Username</th>
-              <th>Email</th>
-              <th>Role</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map((user) => (
-              <UserTableRow
-                key={user.id}
-                id={user.id}
-                username={user.username}
-                email={user.email}
-                role={user.role}
-                onRoleChange={handleRoleChange}
-                onDelete={() => handleDelete(user.id)}
-              />
-            ))}
-          </tbody>
-        </table>
+        <div className="title-container">
+          <div>
+            <h1>Korisnici</h1>
+            <p>Pregledajte i upravljajte korisnicima</p>
+          </div>
+        </div>
+        {!users.length > 0 ? (
+          <p>Trenutno nema evidentiranih korisnika</p>
+        ) : (
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Uloga</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <UserTableRow
+                  key={user.id}
+                  id={user.id}
+                  username={user.username}
+                  email={user.email}
+                  role={user.role}
+                  onRoleChange={handleRoleChange}
+                />
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
     </div>
   );
