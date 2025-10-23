bc075ed2d6c9e3ef822a44a417a057be1a187b22
Uros <milinovicuros@gmail.com>
2025-10-23 06:11:16 +0200

    feat: dodat orderService

---
+++ gozba_na_klik/src/components/service/orderService.jsx
@@ -0,0 +1,24 @@
+import AxiosConfig from "../../config/axios.config";
+
+const RESOURCE = "api/orders";
+
+export async function getActiveOrderByCourier(courierId) {
+  const response = await AxiosConfig.get(
+    `${RESOURCE}/courier/${courierId}/active-pickup`
+  );
+  return response.data;
+}
+
+export async function updateOrderToInDelivery(orderId) {
+  const response = await AxiosConfig.put(
+    `${RESOURCE}/${orderId}/status/to-in-delivery`
+  );
+  return response.data;
+}
+
+export async function updateOrderToDelivered(orderId) {
+  const response = await AxiosConfig.put(
+    `${RESOURCE}/${orderId}/status/to-delivered`
+  );
+  return response.data;
+}
